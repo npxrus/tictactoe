f2745b912da51424d32c8f8a898d2d06f8b013a7
Evgeniy Mironov <npxrus@yandex.com>
2022-11-04 18:38:40 +0300

    feat: add scripts files

---
+++ scripts/app.js
@@ -0,0 +1,5 @@
+const btnEditPlayerOne = document.querySelector('#edit-player1');
+const btnEditPlayerTwo = document.querySelector('#edit-player2');
+
+btnEditPlayerOne.addEventListener('click', openGameConfig);
+btnEditPlayerTwo.addEventListener('click', openGameConfig);
