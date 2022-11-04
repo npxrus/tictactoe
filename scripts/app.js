50c672622070018963a50b5589face0030537a6d
Evgeniy Mironov <npxrus@yandex.com>
2022-11-04 18:56:25 +0300

    feat: add open/closing modal functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,12 @@
+const backdrop = document.querySelector('.backdrop');
+const configOverlay = document.querySelector('.modal');
+
 const btnEditPlayerOne = document.querySelector('#edit-player1');
 const btnEditPlayerTwo = document.querySelector('#edit-player2');
+const btnCancelConfig = document.querySelector('#config-cancel');
 
 btnEditPlayerOne.addEventListener('click', openGameConfig);
 btnEditPlayerTwo.addEventListener('click', openGameConfig);
+
+btnCancelConfig.addEventListener('click', closeGameConfig);
+backdrop.addEventListener('click', closeGameConfig);
--- scripts/config.js
+++ scripts/config.js
@@ -1,1 +1,9 @@
-function openGameConfig() {}
+function openGameConfig() {
+  configOverlay.style.display = 'block';
+  backdrop.style.display = 'block';
+}
+
+function closeGameConfig() {
+  configOverlay.style.display = 'none';
+  backdrop.style.display = 'none';
+}
